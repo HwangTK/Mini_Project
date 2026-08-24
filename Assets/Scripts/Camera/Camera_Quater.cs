ff6a59770e80a44b870a32cda841b4036ff83631
HwangTK <sungwoo410@naver.com>
2026-08-24 10:53:24 +0900

    Initial commit

---
+++ Assets/Scripts/Camera/Camera_Quater.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Quater : MonoBehaviour
+{
+    [Header("카메라")]
+    [SerializeField] private Camera _camera;
+
+
+    [Header("플레이어")]
+    [SerializeField] private Transform _player;
+
+
+
+    void Start()
+    {
+        
+    }
+
+
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+
+
+    void Start()
+    {
+        
+    }
+
+
+
+    void Update()
+    {
+        
+    }
+}
