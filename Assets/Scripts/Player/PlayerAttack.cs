429609b3db2b47be4170eb7c3528ce7efd90a3ee
HwangTK <sungwoo410@naver.com>
2026-08-24 22:03:10 +0900

    Add Particles
    particles

---
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    [SerializeField] WeaponBase _currentWeapon;
+
+
+
+    void Start()
+    {
+        
+    }
+
+
+
+    void Update()
+    {
+        Attack();
+    }
+
+
+    private void Attack()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("АјАн");
+        }
+    }
+
+}
