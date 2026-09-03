1c0b4e9c70b2f22fa77e9f73d683b13ca6d8a86d
HwangTK <sungwoo410@naver.com>
2026-09-03 18:43:15 +0900

    Add Assets, Made Items, Recipe

--- Assets/Scripts/Craft/CraftManager.cs
+++ Assets/Scripts/Craft/CraftManager.cs
@@ -1,22 +1,79 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CraftManager : MonoBehaviour
 {
     [SerializeField] private CraftSlot[] _craftSlots;
 
+    [SerializeField] private Inventory _inventory;
+    [SerializeField] private CraftRecipe[] _recipes;
+    [SerializeField] private TMP_Text _resultText;
 
 
     public void Craft()
     {
+        List<ItemData> currentItems = new List<ItemData>();
+        
+
         for (int i = 0; i < _craftSlots.Length; i++)
         {
-            if (!_craftSlots[i].IsEmpty)
+            if (_craftSlots[i].IsEmpty)
             {
-                Debug.Log(_craftSlots[i].ItemData.itemName);
+                continue;
+            }
+
+            currentItems.Add(_craftSlots[i].ItemData);
+            
+
+        }
+
+        for (int i = 0; i < _recipes.Length; i++)
+        {
+            CraftRecipe recipe = _recipes[i];
+
+            if (currentItems.Count != recipe.materials.Length)
+            {
+                continue;
+            }
+
+            bool isMatch = true;
+
+            for (int j = 0; j < recipe.materials.Length; j++)
+            {
+                ItemData recipeItem = recipe.materials[j];
+
+                if (!currentItems.Contains(recipeItem))
+                {
+                    isMatch = false;
+                    break;
+                }
+            }
+
+
+            if (isMatch)
+            {
+                Debug.Log("제작 성공");
+                _resultText.text = "제작 성공";
+
+                _inventory.AddItem(recipe.resultItem);
+
+                for (int j = 0; j < _craftSlots.Length; j++)
+                {
+                    if (!_craftSlots[j].IsEmpty)
+                    {
+                        _craftSlots[j].RemoveItem();
+                    }
+                }
+
+                return;
             }
         }
+        
+        Debug.Log("조합 실패");
+        _resultText.text = "조합 실패";
+
     }
 
 
---
+++ Assets/Scripts/Craft/CraftRecipe.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Craft Recipe")]
+public class CraftRecipe : ScriptableObject
+{
+    public ItemData[] materials;
+    public ItemData resultItem;
+
+
+
+
+
+}
